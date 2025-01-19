7814f0afa46164e751c3a989f23958dfe9ef2228
Zakaria Abdisalam <47097630+zabdisalam@users.noreply.github.com>
2025-01-18 21:07:19 -0500

    added cors policies

--- src/main.ts
+++ src/main.ts
@@ -6,6 +6,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.enableCors({
+    origin: "*",
+    allowedHeaders: "*",
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE'
+  })
+  await app.listen(process.env.PORT ?? 8000);
 }
 bootstrap();
